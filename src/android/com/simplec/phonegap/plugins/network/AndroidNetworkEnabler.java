c713e84b9f5c209fa547a2de4cd161631e79014d
dnelson_simplec <dnelson@simplec.com>
2014-09-05 15:42:01 -0400

    .

--- src/android/com/simplec/phonegap/plugins/network/AndroidNetworkEnabler.java
+++ src/android/com/simplec/phonegap/plugins/network/AndroidNetworkEnabler.java
@@ -1,25 +1,77 @@
 package com.simplec.phonegap.plugins.network;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.cordova.CordovaInterface;
-import android.util.Log;
-import android.os.Build;
-
-import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 
-public class AndroidNetworkEnabler extends CordovaPlugin {
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
 
-    private static final String LOG_TAG = "AndroidNetworkEnabler";
+public class AndroidNetworkEnabler extends CordovaPlugin implements Runnable {
 
+	private static final String LOG_TAG = "AndroidNetworkEnabler";
+	private Object syncObj = new Object();
+	private ScheduledExecutorService scheduledExecutorService = null;
 
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
-        
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-	        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+		super.initialize(cordova, webView);
+		Log.i(LOG_TAG, "In initialize method");
+		schedule();
+	}
 	
-	        Log.d(LOG_TAG, "setMediaPlaybackRequiresUserGesture: FALSE");
-        }
+	@Override
+	public void onDestroy() {
+		Log.i(LOG_TAG, "In onDestroy method");
+		unschedule();
+		super.onDestroy();
+	}
 
-    }
+	@Override
+	public void run() {
+		Log.i(LOG_TAG, "In run method");
+		ConnectivityManager connManager = (ConnectivityManager) webView
+				.getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo current = connManager.getActiveNetworkInfo();
+
+		if (current != null && current.isConnectedOrConnecting()) {
+			Log.i(LOG_TAG, "There is an active connection");
+			return;
+		}
+
+		WifiManager wifiManager = (WifiManager) webView.getContext()
+				.getSystemService(Context.WIFI_SERVICE);
+		if (!wifiManager.isWifiEnabled()) {
+			Log.i(LOG_TAG, "Enabling Wifi");
+			boolean success = wifiManager.setWifiEnabled(true);
+			Log.i(LOG_TAG, "Enable Wifi returns: " + success);
+		}
+	}
+
+	public void schedule() {
+		unschedule();
+
+		synchronized (syncObj) {
+			scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+			scheduledExecutorService.scheduleAtFixedRate((Runnable) this, 30,
+					30, TimeUnit.SECONDS);
+		}
+	}
+
+	public void unschedule() {
+		synchronized (syncObj) {
+			if (scheduledExecutorService != null) {
+				ScheduledExecutorService exeSvc = scheduledExecutorService;
+				scheduledExecutorService = null;
+				exeSvc.shutdownNow();
+			}
+		}
+	}
 }
